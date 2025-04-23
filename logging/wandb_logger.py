7ce41498ce8bb3bd2144c1421edbea1589e6c47b
FedorZaitsev <91181551+FedorZaitsev@users.noreply.github.com>
2025-04-23 16:11:17 +0300

    Update wandb_logger.py

--- logging/wandb_logger.py
+++ logging/wandb_logger.py
@@ -2,12 +2,9 @@ import wandb
 
 
 class WandBLogger():
-	def __init__(self, key, proj_name, name, cfg, run_id=None):
+	def __init__(self, key, proj_name, name, cfg, *args, **kwargs):
 		wandb.login(key=key)
-		if run_id is None:
-			self.run = wandb.init(project=proj_name, name=name, config=cfg)
-		else:
-			self.run = wandb.init(project=proj_name, name=name, config=cfg, id=run_id, resume='must')
+		self.run = wandb.init(project=proj_name, name=name, config=cfg, *args, **kwargs)
 
 	def log(self, name, value):
 		wandb.log({name : value})
